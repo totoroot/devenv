bf833fd0d085604b59c74433ae7d62d74d411d06
Matthias Thym <git@thym.at>
2024-03-04 11:03:41 +0100

    Update python-poetry example

--- examples/python-poetry/devenv.nix
+++ examples/python-poetry/devenv.nix
@@ -4,12 +4,29 @@
   packages = [
     # A native dependency of numpy
     pkgs.zlib
-
     # A python dependency outside of poetry.
     config.languages.python.package.pkgs.pjsua2
   ];
+
   languages.python = {
     enable = true;
-    poetry.enable = true;
+    poetry = {
+      enable = true;
+      install = {
+        enable = true;
+        installRootPackage = false;
+        onlyInstallRootPackage = false;
+        compile = false;
+        quiet = false;
+        groups = [ ];
+        ignoredGroups = [ ];
+        onlyGroups = [ ];
+        extras = [ ];
+        allExtras = false;
+        verbosity = "no";
+      };
+      activate.enable = true;
+      package = pkgs.poetry;
+    };
   };
 }
